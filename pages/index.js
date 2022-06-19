3967f7f1ed996798de2a4eaf8cf3b51244eea039
zinwin996 <zinwinnaing996@gmail.com>
2022-06-19 17:06:19 +0630

    add a

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import PricingSection from "../components/PricingSection/PricingSection";
 import AppMusicSection from "../components/AppMusicSection/AppMusicSection";
 import AppSection from "../components/AppSection/AppSection";
 import AppTVSection from "../components/AppTVSection/AppTVSection";
@@ -12,6 +13,7 @@ export default function Home() {
         <NavBar />
       </div>
       <HeaderSection />
+      <PricingSection />
       <DeviceSection />
       <AppSection />
       <AppMusicSection />
